d58d7ef522c5e7ade179cdc3f599fa0b48b33137
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-02-27 14:26:11 +0000

    ajout de l'api de google search

---
+++ content/api_image.py
@@ -0,0 +1,31 @@
+#Google search
+import requests
+import random
+import os
+
+print("*************************************" , os.getcwd())
+def searchImage(title):
+    API_KEY = open('content/API_KEY').read()
+    SEARCH_ENIGIME_ID = open('content/SEARCH_ENIGIME_ID').read()
+    searchQurery = title
+    url = 'https://www.googleapis.com/customsearch/v1'
+    params = {
+        'q': searchQurery,
+        'key': API_KEY,
+        'cx': SEARCH_ENIGIME_ID,
+        'searchType': 'image'
+    }
+    allUrls = []
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        urls_response = response.json()['items']
+        for index in range(len(urls_response)):
+            allUrls.append(urls_response[index]['link'])
+        url_send = random.choice(allUrls)
+
+        return url_send
+
+    return None
+
+
+     
--- content/views.py
+++ content/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 # Create your views here.
+from .models import  ImageContent
+from .api_image import searchImage
+
+ur  = searchImage('homme')
+print(ur, "*******************")
+def index(request):
+    image_all = ImageContent.objects.all()
+    if request.method == 'POST':
+        if len(image_all) == 0:
+            image_all.delete()
+
+        try:
+
+            search_img = request.POST["search"]
+            image = searchImage(search_img)
+            title = search_img
+            new_image = ImageContent.objects.create(title=title, url_image=image)
+            new_image.save()
+            
+
+
+        except:
+            
+            messages.info(request, "Erreure verifier votre connextion")
+    context = {"new_image":image_all}       
+    return render(request, 'index.html', context)
